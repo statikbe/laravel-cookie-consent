49f179ef3f582550466cb98e4746b7f2769b949f
Kristof <kristof@statik.be>
2020-02-03 08:12:36 +0100

    init comit + modal to consent to cookies

---
+++ resources/lang/en/texts.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title_cookiebanner' => 'This website uses cookies to improve your browsing experience. By clicking on ‘Agree’, you accept the use of these cookies.',
+    'body_notice_gdpr' => 'Our website keeps three levels of cookies. You can adjust your preferences at any time. If you want more information about what cookies are and which cookies we collect, please read our <a href=":policyUrl">cookie policy</a>.',
+    'settings_notice_gdpr' => 'Cookie settings',
+    'accept_notice_gdpr' => 'Agreed',
+    'title_modal_gdpr' => 'Cookie settings',
+    'title_essential_modal_gdpr' => 'Essential cookies',
+    'text_essential_modal_gdpr' => 'Functional cookies are essential cookies that ensure that de website functions properly and that your preferences (e.g.  language, region) are saved.',
+    'title_performance_modal_gdpr' => 'Analytical cookies',
+    'text_performance_modal_gdpr' => 'Analytical cookies allow us to analyse website use and to improve the visitor\'s experience.',
+    'title_marketing_modal_gdpr' => 'Marketing cookies',
+    'text_marketing_modal_gdpr' => 'The aim of marketing cookies is to personalise your experience and to send you relevant content and offers, on this website and other websites.',
+    'close_modal_gdpr' => 'Save',
+    'active_gdpr' => 'Active',
+    'alwaysactive_gdpr' => 'Always active',
+    'notactive_gdpr' => 'Non-active',
+    'title_gdpr' => 'This website uses cookies'
+];
