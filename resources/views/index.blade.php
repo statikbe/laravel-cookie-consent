dba951ce60e5385373ac3f1bb8f9d0b3b8fa42be
Kristof <kristof@statik.be>
2020-09-04 07:35:06 +0200

    Updated tpl, removed obsolete parenthesis

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -42,7 +42,7 @@
                 @lang('cookie-consent::texts.settings_title')
             </h2>
             <p id="lcc-modal-settings-desc" class="lcc-text">
-                @lang('cookie-consent::texts.settings_text', [ 'policyUrl' => config("cookie-consent.policy_url_$locale")]))
+                @lang('cookie-consent::texts.settings_text', [ 'policyUrl' => config("cookie-consent.policy_url_$locale")])
             </p>
             <div class="lcc-modal__section lcc-u-text-center">
                 <button type="button" class="lcc-button js-lcc-accept">
