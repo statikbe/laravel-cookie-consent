7b38e40fe2a101cbdc0fa94f0fc315fb86aaea4d
Vorja <truestylegs@gmail.com>
2022-12-27 10:51:57 +0100

    feat(lang): add spanish translation

---
+++ resources/lang/es/texts.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'alert_accept' => 'Aceptar todas las cookies',
+    'alert_essentials_only' => 'Aceptar solo las cookies necesarias',
+    'alert_settings' => 'Ajusta tus preferencias',
+    'alert_text' => 'Al elegir "Aceptar todas las cookies", acepta el uso de cookies para ayudarnos a brindarle una mejor experiencia de usuario y analizar el uso del sitio web. Al hacer clic en "Ajuste sus preferencias" puede elegir qué cookies permitir. Solo las cookies esenciales son necesarias para el correcto funcionamiento de nuestro sitio web y no pueden ser rechazadas',
+    'alert_title' => 'Esta página web usa cookies',
+    'alert_essential_only' => 'Aceptar solo las cookies necesarias',
+    'setting_analytics' => 'Cookies analíticas',
+    'setting_analytics_text' => 'nos permite analizar el uso del sitio web y mejorar la experiencia del visitante',
+    'setting_essential' => 'Cookies esenciales',
+    'setting_essential_text' => 'son necesarios por razones técnicas. Sin ellos, es posible que este sitio web no funcione correctamente.',
+    'setting_funcional' => 'Cookies funcionales',
+    'setting_funcional_text' => 'son necesarios para una funcionalidad específica en el sitio web. Sin ellos, algunas funciones pueden estar deshabilitadas.',
+    'setting_marketing' => 'Cookies de marketing',
+    'setting_marketing_text' => 'permítanos personalizar su experiencia y enviarle contenido y ofertas relevantes, en este sitio web y en otros sitios web',
+    'settings_accept_all' => 'Aceptar todas las cookies',
+    'settings_cancel' => 'Cancelar',
+    'settings_close' => 'Cerrar',
+    'settings_save' => 'Guardar mi selección',
+    'settings_title' => 'Configuración de cookies',
+    'settings_text' => 'Nuestro sitio web almacena cuatro tipos de cookies. En cualquier momento puede elegir qué cookies acepta y cuáles rechaza. Puede obtener más información sobre qué son las cookies y qué tipos de cookies almacenamos en nuestra <a href=":policyUrl" target="_blank" class="underline hover:no-underline">Política de cookies</a>.'
+];
