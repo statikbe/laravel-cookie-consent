297a9be5058362f97e60f70e57c82b1d2fee57df
Guillaume ernst <guillaume.ernst@infernal.media>
2023-09-13 11:46:30 -0400

    feat(view): update html

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,12 @@
     $locale = $app->getLocale();
 @endphp
 
-<div role="dialog" aria-labelledby="lcc-modal-alert-label" aria-describedby="lcc-modal-alert-desc" aria-modal="true" class="lcc-modal lcc-modal--alert js-lcc-modal js-lcc-modal-alert" style="display: none;"
+<div role="dialog"
+     aria-labelledby="lcc-modal-alert-label"
+     aria-describedby="lcc-modal-alert-desc"
+     aria-modal="true"
+     class="lcc-modal lcc-modal--alert js-lcc-modal js-lcc-modal-alert"
+     style="display: none;"
      data-cookie-key="{{ config('cookie-consent.cookie_key') }}"
      data-cookie-value-analytics="{{ config('cookie-consent.cookie_value_analytics') }}"
      data-cookie-value-marketing="{{ config('cookie-consent.cookie_value_marketing') }}"
@@ -10,31 +15,43 @@
      data-cookie-value-none="{{ config('cookie-consent.cookie_value_none') }}"
      data-cookie-expiration-days="{{ config('cookie-consent.cookie_expiration_days') }}"
      data-gtm-event="{{ config('cookie-consent.gtm_event') }}"
-     data-ignored-paths="{{ implode(',', config('cookie-consent.ignored_paths', [])) }}"
->
+     data-ignored-paths="{{ implode(',', config('cookie-consent.ignored_paths', [])) }}">
     <div class="lcc-modal__content">
-        <h2 id="lcc-modal-alert-label" class="lcc-modal__title">
+        {{-- <h2 id="lcc-modal-alert-label"
+            class="lcc-modal__title">
             @lang('cookie-consent::texts.alert_title')
-        </h2>
-        <p id="lcc-modal-alert-desc" class="lcc-text">
+        </h2> --}}
+        <p id="lcc-modal-alert-desc"
+           class="lcc-text">
             {!! trans('cookie-consent::texts.alert_text') !!}
         </p>
     </div>
     <div class="lcc-modal__actions">
-        <button type="button" class="lcc-button js-lcc-accept">
+        <button type="button"
+                class="lcc-button js-lcc-accept">
             @lang('cookie-consent::texts.alert_accept')
         </button>
-        <button type="button" class="lcc-button lcc-button--link js-lcc-essentials">
-            @lang('cookie-consent::texts.alert_essential_only')
-        </button>
-        <button type="button" class="lcc-button lcc-button--link js-lcc-settings-toggle">
+        <a type="button"
+           class="lcc-button lcc-button--link js-lcc-essentials">
+            {{-- @lang('cookie-consent::texts.alert_essential_only') --}}
+            {!! trans('cookie-consent::texts.alert_essential_only', ['policyUrl' => config("cookie-consent.policy_url_$locale")]) !!}
+        </a>
+
+        {{-- <button type="button"
+                class="lcc-button lcc-button--link js-lcc-settings-toggle">
             @lang('cookie-consent::texts.alert_settings')
-        </button>
+        </button> --}}
     </div>
 </div>
 
-<div role="dialog" aria-labelledby="lcc-modal-settings-label" aria-describedby="lcc-modal-settings-desc" aria-modal="true" class="lcc-modal lcc-modal--settings js-lcc-modal js-lcc-modal-settings" style="display: none;">
-    <button class="lcc-modal__close js-lcc-settings-toggle" type="button">
+<div role="dialog"
+     aria-labelledby="lcc-modal-settings-label"
+     aria-describedby="lcc-modal-settings-desc"
+     aria-modal="true"
+     class="lcc-modal lcc-modal--settings js-lcc-modal js-lcc-modal-settings"
+     style="display: none;">
+    <button class="lcc-modal__close js-lcc-settings-toggle"
+            type="button">
         <span class="lcc-u-sr-only">
             @lang('cookie-consent::texts.settings_close')
         </span>
@@ -42,20 +59,27 @@
     </button>
     <div class="lcc-modal__content">
         <div class="lcc-modal__content">
-            <h2 id="lcc-modal-settings-label" class="lcc-modal__title">
+            <h2 id="lcc-modal-settings-label"
+                class="lcc-modal__title">
                 @lang('cookie-consent::texts.settings_title')
             </h2>
-            <p id="lcc-modal-settings-desc" class="lcc-text">
-                {!! trans('cookie-consent::texts.settings_text', [ 'policyUrl' => config("cookie-consent.policy_url_$locale")]) !!}
+            <p id="lcc-modal-settings-desc"
+               class="lcc-text">
+                {!! trans('cookie-consent::texts.settings_text', ['policyUrl' => config("cookie-consent.policy_url_$locale")]) !!}
             </p>
             <div class="lcc-modal__section lcc-u-text-center">
-                <button type="button" class="lcc-button js-lcc-accept">
+                <button type="button"
+                        class="lcc-button js-lcc-accept">
                     @lang('cookie-consent::texts.settings_accept_all')
                 </button>
             </div>
             <div class="lcc-modal__section">
-                <label for="lcc-checkbox-essential" class="lcc-label">
-                    <input type="checkbox" id="lcc-checkbox-essential" disabled="disabled" checked="checked">
+                <label for="lcc-checkbox-essential"
+                       class="lcc-label">
+                    <input type="checkbox"
+                           id="lcc-checkbox-essential"
+                           disabled="disabled"
+                           checked="checked">
                     <span>@lang('cookie-consent::texts.setting_essential')</span>
                 </label>
                 <p class="lcc-text">
@@ -63,8 +87,12 @@
                 </p>
             </div>
             <div class="lcc-modal__section">
-                <label for="lcc-checkbox-funtcional" class="lcc-label">
-                    <input type="checkbox" id="lcc-checkbox-funtcional" disabled="disabled" checked="checked">
+                <label for="lcc-checkbox-funtcional"
+                       class="lcc-label">
+                    <input type="checkbox"
+                           id="lcc-checkbox-funtcional"
+                           disabled="disabled"
+                           checked="checked">
                     <span>@lang('cookie-consent::texts.setting_functional')</span>
                 </label>
                 <p class="lcc-text">
@@ -72,8 +100,10 @@
                 </p>
             </div>
             <div class="lcc-modal__section">
-                <label for="lcc-checkbox-analytics" class="lcc-label">
-                    <input type="checkbox" id="lcc-checkbox-analytics">
+                <label for="lcc-checkbox-analytics"
+                       class="lcc-label">
+                    <input type="checkbox"
+                           id="lcc-checkbox-analytics">
                     <span>@lang('cookie-consent::texts.setting_analytics')</span>
                 </label>
                 <p class="lcc-text">
@@ -81,8 +111,10 @@
                 </p>
             </div>
             <div class="lcc-modal__section">
-                <label for="lcc-checkbox-marketing" class="lcc-label">
-                    <input type="checkbox" id="lcc-checkbox-marketing">
+                <label for="lcc-checkbox-marketing"
+                       class="lcc-label">
+                    <input type="checkbox"
+                           id="lcc-checkbox-marketing">
                     <span>@lang('cookie-consent::texts.setting_marketing')</span>
                 </label>
                 <p class="lcc-text">
@@ -92,11 +124,14 @@
         </div>
     </div>
     <div class="lcc-modal__actions">
-        <button type="button" class="lcc-button js-lcc-settings-save">
+        <button type="button"
+                class="lcc-button js-lcc-settings-save">
             @lang('cookie-consent::texts.settings_save')
         </button>
     </div>
 </div>
 
-<div class="lcc-backdrop js-lcc-backdrop" style="display: none;"></div>
-<script type="text/javascript" src="{{ asset("vendor/cookie-consent/js/cookie-consent.js") }}"></script>
+<div class="lcc-backdrop js-lcc-backdrop"
+     style="display: none;"></div>
+<script type="text/javascript"
+        src="{{ asset('vendor/cookie-consent/js/cookie-consent.js') }}"></script>
