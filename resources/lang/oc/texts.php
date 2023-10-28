73b01b7cb2897235f0c3c793a60075c2ea80497b
Mejans <61360811+Mejans@users.noreply.github.com>
2023-10-28 09:42:47 +0200

    Adds Occitan locale

---
+++ resources/lang/oc/texts.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'alert_accept' => 'Acceptar totes los cookies',
+    'alert_essentials_only' => 'Acceptar sonque los cookies necessaris',
+    'alert_settings' => 'Ajustar vòstras preferéncias',
+    'alert_text' => 'En causissent « Acceptar totes los cookies » indicatz que sètz d’acòrd amb l’utilizacion dels cookies per nos ajudar a vos provesir una melhora experiéncia utilizaire e a analisar l’utilizacion del site web. En clicant « Ajustar vòstras preferéncias » podètz causir quines cookies autorizar. Sonque los cookies essencials son necessaris per un foncionament corrècte de nòstre site e se pòdon pas refusar',
+    'alert_title' => 'Aqueste site web utiliza de cookies',
+    'alert_essential_only' => 'Acceptar sonque los cookies necessaris',
+    'setting_analytics' => 'Cookies estatistics',
+    'setting_analytics_text' => 'nos permeton d’analisar l’utilizacion del site web e de melhorar l’experiéncia utilizaire.',
+    'setting_essential' => 'Cookies essencials',
+    'setting_essential_text' => 'son necessaris per de rasons tecnicas. Sens eles, aqueste site web poiriá foncionar pas corrèctament.',
+    'setting_functional' => 'Cookies foncionals',
+    'setting_functional_text' => 'son necessaris per de foncionalitats especificas del site web. Sens eles d’unes foncionalitats pòdon èsser desactivadas.',
+    'setting_marketing' => 'Cookies comercials cookies',
+    'setting_marketing_text' => 'nos permeton de personalizar vòstra experiéncia e de vos enviar de contenguts pertinent, sus aqueste site web e sus d’autres sites.',
+    'settings_accept_all' => 'Acceptar totes los cookies',
+    'settings_cancel' => 'Anullar',
+    'settings_close' => 'Tampar',
+    'settings_save' => 'Enregistrar ma seleccion',
+    'settings_title' => 'Paramètres de cookie',
+    'settings_text' => 'Nòstre site web salva quatre tipes de cookies. Quand volètz podètz causir quines cookies autorizatz e los refusatz. Podètz ne legir mai a prepaus dels cookies e de çò que enregistram dins nòstra <a href=":policyUrl" target="_blank" class="underline hover:no-underline">Politica de confidencialitat</a>.'
+];
